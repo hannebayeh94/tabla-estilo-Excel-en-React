6db63bc86224d6d4d27a55801285928f4d83bb0a
Hanne Bayeh <hannejosebayehvillarreal@gmail.com>
2022-06-09 19:11:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
+import HotTable, { HotColumn } from "@handsontable/react";
+import { registerAllModules } from "handsontable/registry";
+import { registerLanguageDictionary, esMX } from "handsontable/i18n";
+import "handsontable/dist/handsontable.full.css";
 
-function App() {
+// ejecutar para obtener todas lasd las funciones de handsontable
+
+registerAllModules();
+registerLanguageDictionary(esMX, "es-MX");
+
+const App = () => {
+  const [usuarios, setUsuarios] = useState([]);
+  const hotTableComponent = useRef(null);
+
+  useEffect(() => {
+    const getData = () => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((data) => setUsuarios(data));
+    };
+
+    getData();
+  }, []);
+
+  const descargarArchivo = () => {
+    const pluginDescarga =
+      hotTableComponent.current.hotInstance.getPlugin("exportFile");
+    pluginDescarga.downloadFile("csv", {
+      filename: "usuarios",
+      fileExtension: "csv",
+      MimeTypeArray: "text/csv",
+      columnHeaders: true,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Table estilo excel</h1>
+      <button onClick={() => descargarArchivo()}>Descargar Archivo</button>
+      {usuarios && (
+        <HotTable
+          ref={hotTableComponent}
+          language={esMX.languageCode}
+          data={usuarios}
+          licenseKey="non-commercial-and-evaluation"
+          colHeaders={true}
+          rowHeaders={true}
+          columnSorting={true}
+          mergeCells={true}
+          contextMenu={["row_above", "row_below"]}
+          readOnly={false}
         >
-          Learn React
-        </a>
-      </header>
+          <HotColumn data="id" title="Identificacion" readOnly={true} />
+          <HotColumn data="name" title="Nombre" />
+          <HotColumn data="username" title="Usuario" />
+          <HotColumn data="email" title="Correo" />
+          <HotColumn data="address.street" title="Direccion" />
+          <HotColumn data="address.city" title="Ciudad" />
+        </HotTable>
+      )}
     </div>
   );
-}
+};
 
 export default App;
